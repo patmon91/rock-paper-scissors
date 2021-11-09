0e7c1881cacdeb8b39e108fc886c8e302b017d58
Patrick Monette <p.monette@hotmail.com>
2021-11-09 09:20:13 -0800

    added userChoice function and play round function that returns whether you win ort lose based on userChoice and  what the computer plays

--- scripts.js
+++ scripts.js
@@ -1,15 +1,47 @@
-function computerPlay () {
+function computerPlay() {
     let randomNumber = Math.floor(Math.random() * 4);
         if (randomNumber == 1) {
-            compChoice = "Scissors"; 
+            compChoice = "scissors"; 
         }
         else if (randomNumber == 2) {
-            compChoice = "Rock";
+            compChoice = "rock";
         }
         else {
-            compChoice = "Paper";
+            compChoice = "paper";
         }
     return compChoice
 }
 
-console.log(computerPlay())
+function userChoice() {
+    const choice = prompt("Do you choose Rock, Paper and Scissors?").toLowerCase();
+    return choice
+}
+
+function playRound(computerSelection, playerSelection) {
+    //rock choice
+    if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You win! Rock beats Scissors";
+    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        return "You lose! Paper beats Rock";
+    } else if (playerSelection == 'rock' && computerSelection == 'rock') {
+        return 'Its a Draw! Play Again'
+    }
+    //paper choice
+    if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return "you Win! Paper beats Rock";
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        return 'You lose! Scissors beats Paper';
+    } else if (playerSelection == 'paper' && computerSelection == 'paper') {
+        return 'Its a Draw! Play Again'
+    }
+    //scissors choice
+    if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        return "You win! scissors beats Paper";
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        return 'You lose! ROck beats Scissors';
+    } else if (playerSelection == 'scissors' && computerSelection == 'scissors') {
+        return 'Its a Draw! Play Again'
+    } else {
+        return "Something Went Wrong"
+    }
+}
